d68176bc2615f686cb6e007a269a9d3c7c86c8bc
Jerome Biotidara <jeromebiotidara@gmail.com>
2015-04-26 17:29:24 -0600

    Enabled on the fly calculations. To be used for rigids. Saving before splitting up rigid node and Non-rigid nodes

--- DB_SubCalendarEventFly.cs
+++ DB_SubCalendarEventFly.cs
@@ -8,6 +8,8 @@ namespace TilerFront
 {
     public class DB_SubCalendarEventFly:SubCalendarEvent
     {
+        DB_SubCalendarEventFly()
+        { }
         internal DB_SubCalendarEventFly(EventID EventIDData, string Name, DateTimeOffset StartData, DateTimeOffset EndData, int PriorityData, Location_Elements LocationData, DateTimeOffset OriginalStartData, TimeSpan EventPrepTimeData, TimeSpan Event_PreDeadlineData, bool EventRigidFlagData, EventDisplay UiData, MiscData NoteData, bool CompletionFlagData, Procrastination ProcrastinationData, NowProfile NowProfileData, TimeLine CalendarEventRangeData, bool FromRepeatFlagData, bool ElapsedFlagData, bool EnabledFlagData, List<string> UserIDData,long Sequence)
         {
             this.BusyFrame = new BusyTimeLine(EventIDData.ToString(), StartData, EndData);
@@ -40,6 +42,30 @@ namespace TilerFront
 
             this.UserIDs = UserIDData;
             this.OriginalStart = OriginalStartData;
+        }
+
+        internal void InitializeDisabled(SubCalendarEvent SubCalendarEventData)
+        {
+            if (!SubCalendarEventData.isEnabled)
+            {
+                TimeSpan SPanShift = SubCalendarEventData.Start - this.Start;
+                this.shiftEvent(SPanShift, true);
+                this.Enabled = SubCalendarEventData.isEnabled;
+                return;
+            }
+            throw new Exception("Trying to set undelete event as deleted, check DB_SubCalendarEventFly");
+        }
+
+        internal void InitializeCompleted(SubCalendarEvent SubCalendarEventData)
+        {
+            if (SubCalendarEventData.isComplete)
+            {
+                TimeSpan SPanShift = SubCalendarEventData.Start - this.Start;
+                this.shiftEvent(SPanShift, true);
+                this.Complete = SubCalendarEventData.isComplete;
+                return;
+            }
+            throw new Exception("Trying to set uncomplete event as completed, check DB_SubCalendarEventFly");
         }
     }
 }
