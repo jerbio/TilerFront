d028d2d1ccfd008e1f94f8bda459a222153e58c7
Jerome Biotidara <Jeromebiotidara@gmail.com>
2018-10-10 18:24:05 -0600

    Restarting tiler server

--- Migrations/201810100708186_initial.Designer.cs
+++ Migrations/201810100708186_initial.Designer.cs
@@ -6,14 +6,14 @@ namespace TilerFront.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class addedAbilityForPauseTime : IMigrationMetadata
+    [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(addedAbilityForPauseTime));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201605300652203_addedAbilityForPauseTime"; }
+            get { return "201810100708186_initial"; }
         }
         
         string IMigrationMetadata.Source
