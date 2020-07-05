1228176cf2667fe4eebd0e3040a77ebdce59ac1c
jerbio <jeromebiotidara@gmail.com>
2020-07-05 01:31:15 -0600

    Code clean up and laptop deployment

--- App_Start/IdentityConfig.cs
+++ App_Start/IdentityConfig.cs
@@ -187,33 +187,4 @@ namespace TilerFront
             return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
         }
     }
-
-    //public class AuthRepository : IDisposable
-    //{
-    //    private TilerDbContext _ctx;
-
-    //    private UserManager<IdentityUser> _userManager;
-
-    //    public AuthRepository()
-    //    {
-    //        _ctx = new TilerDbContext();
-    //        _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_ctx));
-    //    }
-
-
-    //    public async Task<IdentityUser> FindUser(string userName, string password)
-    //    {
-    //        IdentityUser user = await _userManager.FindAsync(userName, password);
-
-    //        return user;
-    //    }
-
-    //    public void Dispose()
-    //    {
-    //        _ctx.Dispose();
-    //        _userManager.Dispose();
-
-    //    }
-    //}
-
 }
